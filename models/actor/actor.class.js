d341cfdac4118203c64e7ab42c81ff9ad1edee04
Marco Elste <marco.elste@web.de>
2025-08-12 22:06:02 +0200

    feat: Updated actor.class.js: DrawableObject.loadImage()

---
+++ models/actor/actor.class.js
@@ -0,0 +1,33 @@
+
+/** An object, which is drwan */
+export class DrawableObject {
+    
+    x;
+    y;
+    width;
+    height;
+    img;
+
+    /**
+     * Creates an object to draw.
+     * @param {number} x - X-Pos of object.
+     * @param {number} y - Y-Pos of object.
+     * @param {number} width - Width of object.
+     * @param {number} height - Height of object.
+     */
+    constructor (x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height
+        this.img = new Image();
+    }
+
+    /**
+     * Image for draw.
+     * @param {string} path - Path of Image.
+     */
+    loedImage(path) {
+        this.img.src = path;
+    }
+}
