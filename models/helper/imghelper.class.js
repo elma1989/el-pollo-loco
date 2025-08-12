c994fd296a79ae6a538cd4da3cc0cfbd71cff3a5
Marco Elste <marco.elste@web.de>
2025-08-12 08:29:49 +0200

    feat: Updated imghelper.class.js: ImgHelper.BACKGROUND

---
+++ models/helper/imghelper.class.js
@@ -0,0 +1,22 @@
+export class ImgHelper {
+    static BACKGROUND = {
+        air: 'assets/img/background/air.png',
+        layers: [
+            [
+                'assets/img/background/layer-1/1.png',
+                'assets/img/background/layer-1/2.png'
+            ],[
+                'assets/img/background/layer-2/1.png',
+                'assets/img/background/layer-2/2.png'
+            ],[
+                'assets/img/background/layer-3/1.png',
+                'assets/img/background/layer-3/2.png'
+            ]
+        ],
+        clouds: {
+            samll: 'assets/img/background/clouds/small.png',
+            medium: 'assets/img/background/clouds/medium.png',
+            large: 'assets/img/background/clouds/large.png'
+        }
+    }
+}
