7e29d3876bd0b20ef7cccea4d265ed6b89a612c2
Marco Elste <marco.elste@web.de>
2025-08-12 09:17:12 +0200

    feat: Updated imghelper.class.js: ImgHelper.PEPE

--- models/helper/imghelper.class.js
+++ models/helper/imghelper.class.js
@@ -19,4 +19,63 @@ export class ImgHelper {
             large: 'assets/img/background/clouds/large.png'
         }
     }
+    static PEPE = {
+        idle: [
+            'assets/img/pepe/idle/I-1.png',
+            'assets/img/pepe/idle/I-2.png',
+            'assets/img/pepe/idle/I-3.png',
+            'assets/img/pepe/idle/I-4.png',
+            'assets/img/pepe/idle/I-5.png',
+            'assets/img/pepe/idle/I-6.png',
+            'assets/img/pepe/idle/I-7.png',
+            'assets/img/pepe/idle/I-8.png',
+            'assets/img/pepe/idle/I-9.png',
+            'assets/img/pepe/idle/I-10.png'
+        ],
+        longIdle: [
+            'assets/img/pepe/long_idle/I-11.png',
+            'assets/img/pepe/long_idle/I-12.png',
+            'assets/img/pepe/long_idle/I-13.png',
+            'assets/img/pepe/long_idle/I-14.png',
+            'assets/img/pepe/long_idle/I-15.png',
+            'assets/img/pepe/long_idle/I-16.png',
+            'assets/img/pepe/long_idle/I-17.png',
+            'assets/img/pepe/long_idle/I-18.png',
+            'assets/img/pepe/long_idle/I-19.png',
+            'assets/img/pepe/long_idle/I-20.png'
+        ],
+        walk: [
+            'assets/img/pepe/walk/W-21.png',
+            'assets/img/pepe/walk/W-22.png',
+            'assets/img/pepe/walk/W-23.png',
+            'assets/img/pepe/walk/W-24.png',
+            'assets/img/pepe/walk/W-25.png',
+            'assets/img/pepe/walk/W-26.png',
+        ],
+        jump: [
+            'assets/img/pepe/jump/J-31.png',
+            'assets/img/pepe/jump/J-32.png',
+            'assets/img/pepe/jump/J-33.png',
+            'assets/img/pepe/jump/J-34.png',
+            'assets/img/pepe/jump/J-35.png',
+            'assets/img/pepe/jump/J-36.png',
+            'assets/img/pepe/jump/J-37.png',
+            'assets/img/pepe/jump/J-38.png',
+            'assets/img/pepe/jump/J-39.png',
+        ],
+        hurt: [
+            'assets/img/pepe/hurt/H-41.png',
+            'assets/img/pepe/hurt/H-42.png',
+            'assets/img/pepe/hurt/H-43.png',
+        ],
+        dead: [
+            'assets/img/pepe/dead/D-51.png',
+            'assets/img/pepe/dead/D-52.png',
+            'assets/img/pepe/dead/D-53.png',
+            'assets/img/pepe/dead/D-54.png',
+            'assets/img/pepe/dead/D-55.png',
+            'assets/img/pepe/dead/D-56.png',
+            'assets/img/pepe/dead/D-57.png'
+        ]
+    }
 }
