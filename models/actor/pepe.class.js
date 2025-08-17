250f45581d1bbcffad5426e0da46a1da10428cf0
Marco Elste <marco.elste@web.de>
2025-08-17 22:02:42 +0200

    feat: Updated pepe.class.js: Pepe()

---
+++ models/actor/pepe.class.js
@@ -0,0 +1,38 @@
+import { MortalActor } from './actor.class.js';
+import { ImgHelper } from '../helper/imghelper.class.js';
+import { IntervalHub } from '../helper/intervalhub.class.js';
+import { Level } from '../world/level.class.js';
+
+/** Represents the main-character. */
+export class Pepe extends MortalActor {
+
+    // #region Attributes
+
+    // #endregion
+
+    /**
+     * Creates the mein-character Pepe.
+     * @param {Level} level - Level on which Pepe lives.
+     * @param {HTMLElement} canvas - Canvas-Object, on which Pepe is drawn.
+     */
+    constructor(level, canvas) {
+        super(0, 610, 1200, level);
+        this.scale(0.25);
+        this.loadImages(ImgHelper.PEPE.longIdle);
+        this.animate();
+        this.ground(canvas);
+        this.y = this.groundLevel;
+        this.offset.left = 35;
+        this.offset.right = 55;
+        this.offset.top = 155;
+        this.offset.bottom = 40;
+    }
+
+    pepeAni = () => {
+        this.playAnimation(ImgHelper.PEPE.longIdle);
+    }
+
+    animate() {
+        IntervalHub.startInverval(this.pepeAni, 1000 / 2);
+    }
+}
