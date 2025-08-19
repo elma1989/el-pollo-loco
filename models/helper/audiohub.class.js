8a9bb00acdcd32afc0cdf15c265b098d3012df40
Marco Elste <marco.elste@web.de>
2025-08-19 16:48:37 +0200

    feat: Updated audiohub.class.js: AudioHub.PEPE, AudioHub.ENEMY, AudioHub.COLLECTABLES

--- models/helper/audiohub.class.js
+++ models/helper/audiohub.class.js
@@ -19,7 +19,23 @@ export class AudioHub {
     // #region Attributes
     static GAME = new MyAudio('assets/sounds/start.mp3');
     static PEPE = {
-        walk : new MyAudio('assets/sounds/pepe/walk.mp3')
+        walk: new MyAudio('assets/sounds/pepe/walk.mp3'),
+        snoring: new MyAudio('assets/sounds/pepe/snoring.mp3'),
+        jump: new MyAudio('assets/sounds/pepe/jump.wav'),
+        hurt: new MyAudio('assets/sounds/pepe/hurt.mp3'),
+        dead: new MyAudio('assets/sounds/pepe/dead.wav')
+    };
+    static ENEMY = {
+        chick: new MyAudio('assets/sounds/enemy/chick-dead.mp3'),
+        chicken: new MyAudio('assets/sounds/enemy/chicken-dead.mp3'),
+        boss: new MyAudio('assets/sounds/enemy/boss.wav')
+    };
+    static COLLECTABLES = {
+        coin: new MyAudio('assets/sounds/collectables/coin.wav'),
+        bottle: {
+            collect: new MyAudio('assets/sounds/collectables/bottle-collect.wav'),
+            break: new MyAudio('assets/sounds/collectables/bottle-break.mp3')
+        }
     }
     // #endregion
 
