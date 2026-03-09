9cce6031aada57ab3cff7c1ff958dd0bfbe05574
Marco Elste <marco.elste@web.de>
2026-03-09 18:58:53 +0100

    chore: Added class MovableObject (#27)
    feat: Updated MovableOject - Implement act(), move()

---
+++ src/classes/movable-object.ts
@@ -0,0 +1,28 @@
+import { DrawableObject } from "./drawable-object.js";
+
+/** An drawalble object, which can move.*/
+export abstract class MovableObject extends DrawableObject {
+    static fps: number = 60;
+
+    /**
+     * Creates an movable Object.
+     * @param x - X-Position of object.
+     * @param y - Y-Positon of object.
+     * @param width - Width of object.
+     * @param height - Height of object
+     */
+    constructor(x: number, y: number, width: number, height: number) {
+        super(x, y, width, height);
+    }
+
+    /** Will be repeated 60 times per second. */
+    abstract act(): void;
+
+    /**
+     * Moves an Object horizontally.
+    * @param speed - speed in pixel per interval.
+     */
+    protected move(speed: number) {
+        this.x += speed;
+    }
+}
