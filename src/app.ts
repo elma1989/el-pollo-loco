85ed89a2e522e081b390c717b29632b29cb10198
Marco Elste <marco.elste@web.de>
2026-03-10 16:31:44 +0100

    chore: Added class KeyListener
    feat: Updated KeyListener - Implemented logKeys(), loopLogKeys()
    
    feat: Updated KeyListener - Implmented aplyKeyDown(), aplyKeyUp()

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
+import { KeyListener } from "./classes/key-listener.js";
 import { Level } from "./classes/models/level.js";
 
 const level = new Level();
 
 /** Will be executed on load. */
 async function init(): Promise<void> {
+    new KeyListener();
     await level.loadObjects();
     level.startGame();
 }
