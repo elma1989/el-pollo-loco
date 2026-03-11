564fee7c336b7adfee5e957baab16e73f5cb1933
Marco Elste <marco.elste@web.de>
2026-03-11 08:07:30 +0100

    chore: Added class KeyListener (#37)
    feat: Updated KeyListener - Implemented logKeys(), loopLogKeys()
    
    feat: Updated KeyListener - Implmented aplyKeyDown(), aplyKeyUp()

---
+++ src/classes/key-listener.ts
@@ -0,0 +1,57 @@
+import { IntervalHub } from "./interval-hub.js";
+
+export class KeyListener {
+    static KEY = {
+        left:  false,
+        right: false
+    }
+
+    constructor() {
+        this.aplyKeyDown();
+        this.aplyKeyUp();
+    }
+
+    // #region Methods
+    /** Logs all key-states. */
+    private logKeys():void {
+        for (const [key, value] of Object.entries(KeyListener.KEY)) {
+            console.log(`${key}: ${value}`);
+        }
+    }
+
+    /** Starts a loop to log keys. */
+    private loopLogKeys() {
+        IntervalHub.start(this.logKeys, 1000);
+    }
+
+    /** Adds EventListener for key down. */
+    private aplyKeyDown() {
+        window.addEventListener('keydown', e => {
+            switch(e.code) {
+                case 'ArrowLeft':
+                    KeyListener.KEY.left = true;
+                    break;
+
+                case 'ArrowRight':
+                    KeyListener.KEY.right = true;
+                    break;
+            }
+        });
+    }
+
+    /** Adds EventListener for key up. */
+    private aplyKeyUp() {
+        window.addEventListener('keyup', e => {
+            switch(e.code) {
+                case 'ArrowLeft':
+                    KeyListener.KEY.left = false;
+                    break;
+                
+                case 'ArrowRight':
+                    KeyListener.KEY.right = false;
+                    break;
+            }
+        });
+    }
+    // #endregion
+}
