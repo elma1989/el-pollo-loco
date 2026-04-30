53704ec79b71baaad3ee9e6ae5cdb9593fcaec95
Marco Elste <marco.elste@web.de>
2026-04-30 12:22:54 +0200

    feat: Updated UI - Implemented -createElements()
    Updated Game - implemented -handleControlsButton()
    
    feat: Updated Game - implemented -handleControlsCloseButton()
    
    feat: Updated Game - Implmented -showTextButtons(), -hideTextButtons()

--- src/classes/ui/ui.ts
+++ src/classes/ui/ui.ts
@@ -1,12 +1,28 @@
+import { CloseControlButton } from "./btn-close-ctrl.js";
+import { ControlOverlayButton } from "./btn-ctrl-overlay.js";
 import { RunButton } from "./btn-run.js";
+import { ControlOverlay } from "./overlay-control.js";
 
 export class UI {
-    btns: {text: {run: RunButton}}
+    btns: Record<string,any> = {};
+    overlays: Record<string, any> = {};
 
     constructor() {
+        this.createElements();
+    }
+
+    private createElements(): void {
+        this.overlays = {
+            control: new ControlOverlay()
+        }
+
         this.btns = {
             text: {
-                run: new RunButton()
+                run: new RunButton(),
+                controls: new ControlOverlayButton()
+            },
+            close: {
+                controls: new CloseControlButton()
             }
         }
     }
