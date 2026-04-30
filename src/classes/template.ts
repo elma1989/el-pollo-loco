c12644af7e33d8d8bb31a5b861034b4427dde939
Marco Elste <marco.elste@web.de>
2026-04-30 21:29:27 +0200

    style: Updated font.css - Inserted .font-cowbow
    style: Updated const.css - Inserted .full-canvas .round
    
    Updated style.css - set font-size h2
    
    Updated button.css - Inserted styling btn-controls, btn-close
    
    style: Updted const.css - Inserted .w-full .flex
    
    style: Updated overlay - added general overlay style
    
    style: Updated overlay - add style control-overlay

--- src/classes/template.ts
+++ src/classes/template.ts
@@ -5,31 +5,31 @@ export class Template {
 
     static overlay(name:string) {
         return `
-            <header> 
+            <header class="w-full flex"> 
                 <button id="btn-close-${name}" class="btn-close">X</button>
             </header>
-            <div class="overlay-body" id="overlay-${name}-body"></div>
+            <div class="overlay-body w-full flex" id="overlay-${name}-body"></div>
         `
     }
 
     static controlContainer(): string {
-        return `<div id="control-container"></div>`;
+        return `<div id="control-container" class="w-full flex"></div>`;
     }
 
     static controlKey(key: string): string {
         return `
-            <span class="control-key">${key}</span>
+            <div class="control-key">${key}</div>
         `
     }
 
     static controlItem(item: ControlItem, keys: string): string {
         return `
-            <div class="control-item">
-                <div class="control-img-wrapper">
-                    <img class="control-img" src="${item.path}">
+            <div class="control-item flex">
+                <div class="control-img-wrapper w-full flex">
+                    <img class="w-full" src="${item.path}">
                 </div>
                 <h2 class="font-cowboy">${item.action}</h2>
-                <div class="key-wrapper">${keys}</div>
+                <div class="key-wrapper w-full flex">${keys}</div>
             </div>
             `
     }
