abff60774f1ed5fdaa9f3bda4d9c8dc20fa3d8ee
Marco Elste <marco.elste@web.de>
2026-05-08 08:11:43 +0200

    chore: Inserted assets:
       - assetes/sound - Added assets for character
    
       - SoundPathManger - createChacterSoundPaths()

--- src/classes/sound/snd-path-mgr.ts
+++ src/classes/sound/snd-path-mgr.ts
@@ -1,17 +1,20 @@
 export class SoundPathManager {
     private static prefix: string = '';
 
-    private static GAME: Record<string, string> = {}
+    private static GAME: Record<string, string> = {};
+    private static CHARACTER: Record<string, string> = {};
 
     private constructor() {}
 
     static init(): void {
         this.setPrefix();
         this.createGame();
+        this.createCharactersSounds();
     }
 
     static get game(): Record<string, string> { return this.GAME; }
 
+    static get character(): Record<string, string> { return this.CHARACTER; }
 
     private static setPrefix(): void {
         const host = location.hostname;
@@ -26,6 +29,16 @@ export class SoundPathManager {
         this.GAME = {
             music: `${this.prefix}/music.mp3`,
             start: `${this.prefix}/start.mp3`
+        }
+    }
+
+    private static createCharactersSounds(): void {
+        this.CHARACTER = {
+            sleep: `${this.prefix}/character/snoring.mp3`,
+            walk: `${this.prefix}/character/walk.mp3`,
+            jump: `${this.prefix}/character/jump.wav`,
+            hurt: `${this.prefix}/character/hurt.mp3`,
+            dead: `${this.prefix}/character/dead.wav`
         }
     }
     // #endregion
