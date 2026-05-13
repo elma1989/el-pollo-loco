8e65c35b5156f4731b2bd1bf3e042e3a7c1ed5ad
Marco Elste <marco.elste@web.de>
2026-05-13 17:56:49 +0200

    feat: Logic SwitchLandscape
       - Overridden create()
       - Implmented private createImage()

--- src/classes/ui/overlay-landscape.ts
+++ src/classes/ui/overlay-landscape.ts
@@ -5,6 +5,22 @@ import { Template } from "../template.js";
 export class SwitchLandscape extends Overlay {
     constructor() {
         super('switch-landscape');
+        this.create();
     }
 
+    create(): void {
+        if (this.element) this.element.innerHTML = Template.overlay('landscape', '', false);
+        this.createImage();
+    }
+
+    /** Inserts the smartphone image. */
+    createImage(): void {
+        const parent = document.getElementById('overlay-landscape-body');
+        const img = new Image();
+        if (parent) {
+            img.src = ImgHub.SCREEN.landscape;
+            img.id = 'overlay-landscape-img'
+            parent.appendChild(img);
+        }
+    }
 }
