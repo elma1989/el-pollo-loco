d2e198a9b2eb1a5847814bd5b3202cb584213e41
Marco Elste <marco.elste@web.de>
2026-05-13 14:46:38 +0200

    chore: Setup / Assets
       - Updated index.html - Added SwitchLandscape
       - Updated assets - Added smartphone.png
       - Updated ImgHub - Inserted new image
       - Added overlay-landscape.ts - Created class
       - Updated UI - added SwitchLandscape

---
+++ src/classes/ui/overlay-landscape.ts
@@ -0,0 +1,10 @@
+import { ImgHub } from "../img-hub.js";
+import { Overlay } from "../overlay.js";
+import { Template } from "../template.js";
+
+export class SwitchLandscape extends Overlay {
+    constructor() {
+        super('switch-landscape');
+    }
+
+}
